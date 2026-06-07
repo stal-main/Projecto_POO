157b68282fc3bab50537e06489db67c670107658
Kevin Solano <7stalyakov7@gmail.com>
2026-06-07 05:32:27 -0600

    Clase objeto terminada (pero por alguna razon causa errores)

--- src/logica/Object.java
+++ src/logica/Object.java
@@ -1,6 +1,7 @@
 package logica;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Object implements Serializable {
 	
@@ -12,13 +13,25 @@ public class Object implements Serializable {
 	
 	private String description;
 	
-	public Object(String code, String name, String description) {
+	private Type type;
+	
+	private ArrayList<Category> categories;
+	
+	private Loan loan;
+	
+	public Object(String code, String name, String description, Type type) {
 		
 		this.code = code;
 		
 		this.name = name;
 		
 		this.description = description;
+		
+		this.type = type;
+		
+		this.categories = new ArrayList<>();
+		
+		this.loan = null;
 	}
 	
 	public String getCode() {
@@ -30,5 +43,61 @@ public class Object implements Serializable {
 		
 		return name;
 	}
-
+	
+	public void setName(String name) {
+		
+		this.name = name;
+	}
+	
+	public String getDescription() {
+        return description;
+    }
+ 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public Type getType() {
+    	
+    	return type;
+    }
+    
+    public void setType(Type type) {
+    	
+    	this.type = type;
+    }
+    
+    public void addCategory(Category c) {
+    	
+    	if (!categories.contains(c)) {
+    		
+    		categories.add(c);
+    	}
+    }
+    
+    public void deleteCategory(Category c) {
+    	
+    	categories.remove(c);
+    }
+    
+    public ArrayList<Category> getCategories() {
+    	
+    	return categories;
+    }
+    
+    public Loan getLoan() {
+    	
+    	return loan;
+    }
+    
+    public void setLoan(Loan l) {
+    	
+    	this.loan = l;
+    }
+    
+    @Override
+    public String toString() {
+    	
+    	return name + " [" + code + "]";
+    }
 }
