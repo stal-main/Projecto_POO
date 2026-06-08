351cd2de765184c1eae7b0c0f20cc57a1a32a1b9
Kevin Solano <7stalyakov7@gmail.com>
2026-06-07 21:54:41 -0600

    La que controla

--- src/control/Control.java
+++ src/control/Control.java
@@ -1,8 +1,45 @@
 package control;
 
-public class Control {
+import logica.*;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Control implements Serializable {
 	
-	private Control() {
+	private static final long serialVersionUID = 1L;
+	
+	private static final String DATA_ARCHIVE = "data.dat";
+	
+	private ArrayList<Type> types;
+	
+	private ArrayList<Item> items;
+	
+	private ArrayList<Category> categories;
+	
+	private ArrayList<Loan> loans;
+	
+	private ArrayList<Person> people;
+	
+	private Type genericType;
+	
+	public Control() {
+		
+		types = new ArrayList<>();
+		
+		items = new ArrayList<>();
+		
+		categories = new ArrayList<>();
+		
+		loans = new ArrayList<>();
+		
+		people = new ArrayList<>();
+		
+		genericType = new Type("Generic", "System's generic type");
+		
+		types.add(genericType);
+		
 		
 		
 	}
