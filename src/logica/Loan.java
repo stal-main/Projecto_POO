31ec12699c9acab0f326f80e918467d921a95f6d
Kevin Solano <7stalyakov7@gmail.com>
2026-06-07 23:01:19 -0600

    Lo que hacen los bancos (prestamos)

--- src/logica/Loan.java
+++ src/logica/Loan.java
@@ -15,5 +15,83 @@ public class Loan implements Serializable {
 	private ArrayList<Item> items;
 	
 	private Alert alert;
+	
+	public Loan(Person person) {
+		
+		this.creationDate = new Date();
+		
+		this.person = person;
+		
+		this.items = new ArrayList<>();
+		
+		this.alert = null;
+		
+	}
+	
+	public Date getDate() {
+		
+		return creationDate;
+	}
+	
+	public Person getPerson() {
+		
+		return person;
+	}
+	
+	public void setPerson(Person p) {
+		
+		this.person = p;
+	}
+	
+	public Alert getAlert() {
+		
+		return alert;
+	}
+	
+	public void setAlert(Alert a) {
+		
+		this.alert = a;
+	}
+	
+	public void addItem(Item i) {
+		
+		if (!items.contains(i)) {
+			
+			items.add(i);
+			
+			i.setLoan(this);
+		}
+	}
+	
+	public void deleteItem(Item i) {
+		
+		if (items.remove(i)) {
+			
+			i.setLoan(null);
+		}
+	}
+	
+	public ArrayList<Item> getItems() {
+		
+		return items;
+	}
+	
+	public void endLoan() {
+		
+		for (Item i : new ArrayList<>(items)) {
+			
+			i.setLoan(null);
+		}
+		
+		items.clear();
+		
+		alert = null;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "Loan to " + person.getName() + "(" + creationDate + ")";
+	}
 
 }
