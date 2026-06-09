7060a8207b66c05bd0eee9bb77788a1d0873d2e2
Kevin Solano <7stalyakov7@gmail.com>
2026-06-09 00:07:38 -0600

    Update Person.java

--- src/logica/Person.java
+++ src/logica/Person.java
@@ -17,7 +17,56 @@ public class Person implements Serializable {
 	
 	public Person(String name, String phone, String email) {
 		
+		this.name = name;
 		
+		this.phone = phone;
+		
+		this.email = email;
+		
+		this.loans = new ArrayList<>();
+	}
+	
+	public String getName() {
+		
+		return name;
+	}
+	
+	public void setName(String name) {
+		
+		this.name = name;
+	}
+	
+	public String getPhone() {
+		
+		return phone;
+	}
+	
+	public void setPhone(String phone) {
+		
+		this.phone = phone;
+	}
+	
+	public String getEmail() {
+		
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		
+        this.email = email;
+    }
+	
+	public void addLoan(Loan l) {
+		
+		if (!loans.contains(l)) {
+			
+			loans.add(l);
+		}
+	}
+	
+	public void deleteLoan(Loan l) {
+		
+		loans.remove(l);
 	}
 
 	
