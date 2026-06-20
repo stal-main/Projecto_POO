41c79908c45f8fa0dc5aaf235c268a5045019ba4
Kevin Solano <7stalyakov7@gmail.com>
2026-06-20 07:00:25 -0600

    Update TypesPanel.java

--- src/interfaz/TypesPanel.java
+++ src/interfaz/TypesPanel.java
@@ -16,6 +16,14 @@ public class TypesPanel extends JPanel {
 	
 	private Control control;
 	
+	private DefaultTableModel tableModel;
+	
+    private JTable table;
+    
+    private JTextField txtName;
+    
+    private JTextField txtDesc;
+	
 	public TypesPanel(Control control) {
 		
         this.control = control;
@@ -31,10 +39,50 @@ public class TypesPanel extends JPanel {
 	
 	private void buildUI() {
 		
+		String[] columns = {"Name", "Description", "Items"};
+		
+		tableModel = new DefaultTableModel(columns, 0) {
+			
+			public boolean isCellEditable(int r, int c) {
+				
+				return false;
+			}
+		};
+		
+		table = new JTable(tableModel);
+		
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+        table.setRowHeight(24);
+        
+        table.getSelectionModel().addListSelectionListener(e -> fillForm());
+        
+        add(new JScrollPane(table), BorderLayout.CENTER);
+        
+        JPanel form = new JPanel(new GridBagLayout());
+        
+        form.setBorder(BorderFactory.createTitledBorder("Type data"));
+        
+        GridBagConstraints gbc = new GridBagConstraints();
+        
+        gbc.insets = new Insets(5, 5, 5, 5);
+        
+        gbc.anchor = GridBagConstraints.WEST;
+        
+        add(form, BorderLayout.SOUTH);
+		
 	}
 	
 	private void refreshTable() {
 		
 	}
-
+	
+	private void fillForm() {
+		
+	
+	}
+	
+	private void addType() {
+		
+	}
 }
